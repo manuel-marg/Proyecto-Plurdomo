ebb744674ad78b6dd0abdf5a7904f952d1b65218
Manuel_Rodriguez <manuelrodriguez@correo.unimet.edu.ve>
2021-03-04 11:13:18 -0400

    Nueva DB

--- DB.sql
+++ DB.sql
@@ -14,44 +14,20 @@ CREATE TABLE IF NOT EXISTS propietarios (
     primary key(id)
 );
 
-CREATE TABLE IF NOT EXISTS aptos (
-    id int auto_increment,
-    nro int,
-    piso int,
-    id_edf int,
-    id_inmueble int,
-    active boolean,
-    primary key(id)
-);
 
 CREATE TABLE IF NOT EXISTS inmuebles (
     id int auto_increment,
     alicuota int,
+    numero int,
+    nombre varchar(255) not null,
+    piso int,
     saldo int,
-    calle varchar(255) not null,
-    codigo_postal int,
-    zona varchar(255) not null,
     id_propietario int,
-    active boolean,
-    primary key(id)
-);
-
-CREATE TABLE IF NOT EXISTS casas (
-    id int auto_increment,
-    nombre varchar(255) not null,
     id_inmueble int,
+    tipo varchar(255) not null,
     active boolean,
     primary key(id)
 );
 
-CREATE TABLE IF NOT EXISTS edfs (
-    id int auto_increment,
-    nombre varchar(255) not null,
-    active boolean,
-    primary key(id)
-);
 
 ALTER TABLE inmuebles ADD foreign key(id_propietario) references propietarios(id);
-
-ALTER TABLE aptos ADD foreign key(id_inmueble) references inmuebles(id);
-ALTER TABLE casas ADD foreign key(id_inmueble) references inmuebles(id);
