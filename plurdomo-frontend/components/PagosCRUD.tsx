8cab046d46e7a381e61f2047768f86445583f809
JavierJerezAntonetti <61585930+JavierJerezAntonetti@users.noreply.github.com>
2021-03-09 01:16:12 -0400

    fix pagos

--- plurdomo-frontend/components/PagosCRUD.tsx
+++ plurdomo-frontend/components/PagosCRUD.tsx
@@ -10,22 +10,12 @@ const Crud = ({ pagos}) =>(
                     <div className="row">
                         <div className="col-sm-6">
                             <div className="input-group mb-3">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text" id="basic-addon1">
-                                        <i className="fas fa-search"></i>
-                                    </span>
-                                </div>
-                                <input type="text" id="myInput" onKeyUp={Buscar} className="form-control" placeholder="Buscar por concepto..." />
                             </div>
                         </div>
                         <div className="col-sm-6 text-right vertical-center">
-                            <a href="historicopagos" className="btn btn-outline-secondary m-1">
+                            <a href="historicopagos" className="btn btn-outline-secondary m-1 mb-3">
                                 <i className="fas fa-history"></i>
                                 <span> Historico</span>
-                            </a>
-                            <a href="#AddPropietario" className="btn btn-success m-1" data-toggle="modal">
-                                <i className="fas fa-plus"></i>
-                                <span> Agregar</span>
                             </a>
                         </div>
                     </div>
@@ -87,7 +77,6 @@ const Crud = ({ pagos}) =>(
                     </div>
                     <div className="modal-footer">
                         <input type="button" className="btn btn-default" data-dismiss="modal" defaultValue="Cancel" />
-                        <input type="button" onClick={Agregar} data-dismiss="modal" className="btn btn-success" value="Agregar" />
                     </div>
                 </form>
             </div>
@@ -120,63 +109,6 @@ const Crud = ({ pagos}) =>(
 </div>
 )
 
-
-function Buscar() { // Esta funcion funciona como una especie de filtro en la tabla y simulamos una busqueda por nombre
-    // Declare variables
-    var input, filter, table, tr, td, i, txtValue;
-    input = document.getElementById("myInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("myTable");
-    tr = table.getElementsByTagName("tr");
-  
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[1]; // Establecemos que columna buscara y inicia en 0
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }
-    }
-  }
-
-async function Agregar() { // Funcion para agregar
-    var pago = {monto: "", fecha: "", dia: 0, mes: 0, año: 0, id_factura:0, pendiente:true, pagado:false, active: true}; 
-    pago.monto = (document.getElementById("monto") as HTMLInputElement).value;
-    pago.fecha = (document.getElementById("fecha") as HTMLInputElement).value;
-    var fechaArray = pago.fecha.split("-");
-    pago.dia = parseInt(fechaArray[2]);
-    pago.mes = parseInt(fechaArray[1]);;
-    pago.año = parseInt(fechaArray[0]);;
-    pago.id_factura=1
-    const createPago = await fetch('http://localhost:4000/graphql', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ query: `
-        mutation{
-            createPago(monto: ${pago.monto}, dia: ${pago.dia}, mes: ${pago.mes}, anio: ${pago.año}, id_factura: ${pago.id_factura},  pendiente: true, pagado:false, active: true){
-              id
-              monto
-              dia
-              mes
-              anio
-              id_factura
-              pendiente
-              pagado
-              active
-            }
-          }
-        ` }),
-        }) 
-        const respuestaPago = await createPago.json()
-        const pagoCreado = await respuestaPago.data.createPago
-        console.log(pagoCreado)
-
-        location.reload();
-}
 
 function Checkear(id){
     fetch('http://localhost:4000/graphql', {
